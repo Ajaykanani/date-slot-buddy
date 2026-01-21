8709a1cbecbc5b652c7f83c07aa3e0b4e386f53d
ajaykanani <ajaykanani018@gmail.com>
2026-01-21 10:39:59 +0530

    production env managed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,10 +3,10 @@ export const environment = {
   // Get them from: https://app.supabase.com/project/_/settings/api
   // staging ayk
   // production
-  supabaseUrl: "https://qovncmerdxjiyaitgzld.supabase.co",
-  supabaseAnonKey: "sb_publishable_g3NOxP2ff5V9bOxT10u1XA_8Q2m17Ij",
+  // supabaseUrl: "https://qovncmerdxjiyaitgzld.supabase.co",
+  // supabaseAnonKey: "sb_publishable_g3NOxP2ff5V9bOxT10u1XA_8Q2m17Ij",
   // production. ak018
-  // supabaseUrl: "https://vbinintqefozvmjgitmn.supabase.co",
-  // supabaseAnonKey:
-  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiaW5pbnRxZWZvenZtamdpdG1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MzQwODEsImV4cCI6MjA3ODUxMDA4MX0.3hNBPW-bxBHkqq16hKAg4N7uYviMcgix9HWZq8YWYZs",
+  supabaseUrl: "https://vbinintqefozvmjgitmn.supabase.co",
+  supabaseAnonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiaW5pbnRxZWZvenZtamdpdG1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MzQwODEsImV4cCI6MjA3ODUxMDA4MX0.3hNBPW-bxBHkqq16hKAg4N7uYviMcgix9HWZq8YWYZs",
 };
